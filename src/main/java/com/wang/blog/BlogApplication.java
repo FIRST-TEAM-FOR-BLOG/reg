265bdc5ddd28b53e33ed9ad5677e011c2800d2f8
王祥 <wangxiang@znlhzl.com>
2021-04-01 18:08:24 +0800

    init

---
+++ src/main/java/com/wang/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.wang.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
